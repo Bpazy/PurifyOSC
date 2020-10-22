194c7d45e2cdb625cec38d868c0c0afc09d6d37a
ZiYuan <hanziyuan08@outlook.com>
2020-10-22 17:30:11 +0800

    Update PurifyOSC.user.js

--- PurifyOSC.user.js
+++ PurifyOSC.user.js
@@ -1,2 +1,16 @@
-const mulanDoms = document.querySelectorAll('a[title*="木兰"]');
-mulanDoms.forEach(d => d.setAttribute('style', 'filter:blur(3px); pointer-events:none;'));
+
+
+// ==UserScript==
+// @name         PurifyOSC
+// @namespace    https://github.com/Bpazy/PurifyOSC
+// @version      0.0.1
+// @description  Purify OSCHINA
+// @author       Bpazy
+// @match        https://www.oschina.net/*
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const mulanDoms = document.querySelectorAll('a[title*="木兰"]');
+    mulanDoms.forEach(d => d.setAttribute('style', 'filter:blur(3px); pointer-events:none;'));
+})();
